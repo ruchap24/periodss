c0acf5060375af0aff03ffb9d732f38793d43b9b
rucha <patilrucha991@gmail.com>
2025-06-09 19:09:47 +0530

    updated

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -49,6 +49,7 @@ const sheetVariants = cva(
   }
 )
 
+
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
     VariantProps<typeof sheetVariants> {}
